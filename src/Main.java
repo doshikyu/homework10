4560f4f7baf9818103a75ae68542574a53c17444
doshikyu <yu.dashik@gmail.com>
2023-11-30 18:17:24 +0900

    - Tasks 1-3 completed

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("HELLO WORLD!");
+        System.out.println("Задача 1");
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String space = " ";
+        String fullName = lastName + space + firstName + space + middleName;
+        System.out.println("Ф. И. О. сотрудника — " + fullName);
+
+        System.out.println("Задача 2");
+        System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета — " + fullName.toUpperCase());
+
+        System.out.println("Задача 3");
+        fullName = "Иванов Семён Семёнович";
+        System.out.println(fullName.replaceAll("ё", "е"));
     }
 }
